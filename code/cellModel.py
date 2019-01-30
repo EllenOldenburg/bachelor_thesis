27e1582e57634128e9ac07a7116fedec73e3d845
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-01-30 20:10:20 +0100

    new PAR

--- code/cellModel.py
+++ code/cellModel.py
@@ -56,8 +56,8 @@ Constants
 #         'ku': 1,  # mRNA-Ribosomen-Nichtbindungsrate
 #         'M': 10 ** 8,  # total cell mass
 #         'kcm': 0.00599,  # Chloramphenicol-Bindungsrate
-#         'thetax': [1, 1, 1, 1],  #transkriptionswelle #transcriptional energy-thresholds
-#         'wx': [1, 1, 1, 1]  # wt,wm,wr,wq transkriptionsraten #maximal transcription rates
+#         'thetax': [4.38, 4.38, 426.87],# 4.38],  #transkriptionswelle #transcriptional energy-thresholds
+#         'wx': [4.15, 4.15, 930]#, 948.93]  # wt,wm,wr,wq transkriptionsraten #maximal transcription rates
 # }
 
 
